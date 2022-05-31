e6df0cb3c520f419150cd0a06bb71e291d46afdb
Ashot <akuznecov@dunice.net>
2022-05-31 09:41:16 +0300

    Правки по программе "корзина"

--- src/Program_korzina.java
+++ src/Program_korzina.java
@@ -1,23 +1,21 @@
 import java.util.Scanner;
-    public class Program_korzina {
+
+public class Program_korzina {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
-        int kol;
-        kol = 0;
         String[] arr = new String[100];
         for(int i = 0; i < 100; i++){
-            System.out.print("Введите название товара: ");
+            System.out.print("Введите название товара, для завершения оставьте пустую строку: ");
             arr[i] = num.nextLine();
-            kol+=1;
-            System.out.println("Желаете добавить еще товар? Y / N");
-            String sogl = num.nextLine();
-            if (sogl.equals("N")){
+            if (arr[i] == ""){
                 break;
             }
         }
 
-        for(int i = 0; i < kol; i++){
-            System.out.println((i+1) + "-й товар = " +  arr[i]);
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != null && arr[i] != "") {
+                System.out.println((i + 1) + "-й товар = " + arr[i]);
+            }
         }
     }
 }
